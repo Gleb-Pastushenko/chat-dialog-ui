c25a4cb6ed68833555f2d96e4e1763bc34fadc84
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-19 16:02:37 +0300

    Create Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./app.css";
+import { Header } from "./header/header";
 
 export const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 };
