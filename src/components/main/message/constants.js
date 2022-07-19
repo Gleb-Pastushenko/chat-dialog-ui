2ed63a9abc87fab7b94f2864f27569be0b02c00a
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-19 22:43:19 +0300

    Add message component

---
+++ src/components/main/message/constants.js
@@ -0,0 +1,5 @@
+export const CURRENT_USER_AVATAR_URL =
+  "https://randomuser.me/api/portraits/lego/2.jpg";
+
+export const OTHER_USER_AVATAR_URL =
+  "https://randomuser.me/api/portraits/lego/1.jpg";
---
+++ src/components/main/message/message.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./message.css";
+import { CURRENT_USER_AVATAR_URL, OTHER_USER_AVATAR_URL } from "./constants";
+
+export const Message = ({ text, isCurrentUser }) => {
+  return (
+    <div className={`message ${isCurrentUser ? "message_current-user" : ""}`}>
+      <div className="message__avatar">
+        <img
+          className="message__image"
+          src={isCurrentUser ? CURRENT_USER_AVATAR_URL : OTHER_USER_AVATAR_URL}
+          alt="avatar"
+        />
+      </div>
+      <div className="message__text-bubble">{text}</div>
+    </div>
+  );
+};
