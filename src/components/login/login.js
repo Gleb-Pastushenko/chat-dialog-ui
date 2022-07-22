28276e2508170b3d136c5932d14737a95ba9a84b
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-23 01:25:18 +0300

    Add signup button and logic to submit

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,10 +1,38 @@
 import React from "react";
 import "./login.css";
+import { GoogleAuthProvider } from "firebase/auth";
+import { useSelector } from "react-redux";
+import SignupModal from "./signupModal/signupModal";
 
 export const Login = () => {
+  const [open, setOpen] = React.useState(false);
+  const [submitFunc, setSubmitFunc] = React.useState(null);
+  const auth = useSelector((state) => state.firebaseAuth);
+  const provider = new GoogleAuthProvider();
+
+  const login = () => {};
+
+  const signup = () => {};
+
+  const handleLoginButtonClick = () => {
+    setOpen(true);
+    setSubmitFunc(login);
+  };
+
+  const handleSignupButtonClick = () => {
+    setOpen(true);
+    setSubmitFunc(signup);
+  };
+
   return (
     <main className="login">
-      <button className="login__button">Login with Google account</button>
+      <button className="login__button" onClick={handleLoginButtonClick}>
+        Login
+      </button>
+      <button className="login__button" onClick={handleSignupButtonClick}>
+        Sign up
+      </button>
+      <SignupModal open={open} setOpen={setOpen} submitFunc={submitFunc} />
     </main>
   );
 };
