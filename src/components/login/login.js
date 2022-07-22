4a15321f74ed4e44f48f7a604278f1ff0ec02321
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-22 13:30:43 +0300

    Add Login component

---
+++ src/components/login/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./login.css";
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <button className="login__button">Login with Google account</button>
+    </div>
+  );
+};
