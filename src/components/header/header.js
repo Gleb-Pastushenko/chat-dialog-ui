c8d991df36e5d41267e810a0644588cb05c1deee
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-25 13:33:19 +0300

    Add logout button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,44 @@
+import { IconButton } from "@mui/material";
 import React from "react";
 import "./header.css";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useSelector, useDispatch } from "react-redux";
+import { signOut } from "firebase/auth";
 
-export const Header = () => (
-  <header className="header">
-    <h1 className="header__title">SPDU</h1>
-  </header>
-);
+export const Header = () => {
+  const { isLogged, firebaseAuth: auth } = useSelector((state) => state);
+  console.log(`auth: ${auth}`);
+  const dispatch = useDispatch();
+
+  const handleLogoutClick = () => {
+    console.log("handleLogoutClick");
+    signOut(auth)
+      .then(() => {
+        dispatch({ type: "LOGOUT" });
+      })
+      .catch((error) => {});
+  };
+
+  return (
+    <header className="header">
+      <h1 className="header__title">SPDU</h1>
+      {isLogged ? (
+        <IconButton
+          sx={{
+            backgroundColor: "rgba(0,0,0,0.45)",
+            width: "56px",
+            boxShadow: "0 0 7px 2px white",
+          }}
+          onClick={handleLogoutClick}
+        >
+          <LogoutIcon
+            fontSize="large"
+            sx={{
+              color: "white",
+            }}
+          />
+        </IconButton>
+      ) : null}
+    </header>
+  );
+};
