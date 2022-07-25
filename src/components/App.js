95449d1fd9a9094c082ce76d8671b0c86ba66985
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-25 13:32:57 +0300

    Implement real isLogged state via Redux

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import "./app.css";
 import { Header } from "./header/header";
 import { Routes, Route } from "react-router-dom";
 import { PUBLIC_ROUTES, PRIVATE_ROUTES } from "../routes";
+import { useSelector } from "react-redux";
 
 export const App = () => {
-  const LOGGED = false;
+  const LOGGED = useSelector((state) => state.isLogged);
   const routes = LOGGED ? PRIVATE_ROUTES : PUBLIC_ROUTES;
-  console.log(routes);
   return (
     <div className="app">
       <Header />
