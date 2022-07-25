ed25de58543a768d87a668e0316eb24624cb12c5
Gleb Pastushenko <gleb.pastushenkko@gmail.com>
2022-07-25 13:31:43 +0300

    Add firebase config and objects to reducer

--- src/storage.js
+++ src/storage.js
@@ -1,9 +1,32 @@
+// App imports
 import { createStore } from "redux";
 import { INITIAL_MESSAGES, MESSAGES_FOR_REPLY } from "./constants";
+
+// Firebase imports
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ7lBdBI8py75MJW3PAVrXuixIPFc8w20",
+  authDomain: "chat-dialog-ui.firebaseapp.com",
+  projectId: "chat-dialog-ui",
+  storageBucket: "chat-dialog-ui.appspot.com",
+  messagingSenderId: "500462826259",
+  appId: "1:500462826259:web:b8fa7dc7c1871ceb53a0e6",
+  measurementId: "G-PVMRERJZLV",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 const defaultState = {
   messages: INITIAL_MESSAGES,
   messagesForReply: MESSAGES_FOR_REPLY,
+  firebaseAuth: auth,
+  firebaseDB: db,
+  isLogged: false,
 };
 
 const reducer = (state = defaultState, action) => {
@@ -19,6 +42,11 @@ const reducer = (state = defaultState, action) => {
           },
         ],
       };
+    case "LOGIN":
+      return { ...state, isLogged: true };
+
+    case "LOGOUT":
+      return { ...state, isLogged: false };
 
     default:
       return state;
